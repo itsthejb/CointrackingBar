312dfa22d9b14a940ed012e93f5a92843135bfa9
Jonathan Crooke <joncrooke@gmail.com>
2017-12-16 12:41:58 +0100

    Initial Commit

---
+++ CointrackingBar/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  CointrackingBar
+//
+//  Created by Jonathan Crooke on 16/12/2017.
+//  Copyright © 2017 Jonathan Crooke. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
